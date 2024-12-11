da346008f8aeef62e2acd731f1100d98c23e5d9b
Shruti Dhoot <shrutidhoot2001@gmail.com>
2024-12-11 22:02:56 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ContactUs';
+  
+  Contactform: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.Contactform = this.fb.group({
+      firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      lastName: ['', [Validators.pattern('^[a-zA-Z]+$')]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      city: ['', [Validators.required, Validators.minLength(4)]],
+      state: ['', Validators.required],
+      zip: ['', [Validators.required, Validators.pattern('^[0-9]{6}$')]],
+      comments: ['', [Validators.minLength(30)]]
+    });
+  }
+
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.Contactform.invalid) {
+      return;
+    }
+
+    alert('Form Submitted Successfully!');
+  }
+
 }
